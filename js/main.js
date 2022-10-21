7568abff0333c8961d2f524a5167cf8b1a0a07ae
Pavel-Pro <pavelpro@gmail.com>
2022-10-21 14:28:20 +0300

    Menu without animate

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$('.product-menu__link').mouseover(function(){
-    $('.product-menu__item').last().addClass('hover');
-    console.log(1);
-});
-$('.product-menu__link').mouseout(function(){
-    $('.product-menu__item').last().removeClass('hover');
-});
