634ac0f6f4096ccb0acd32541ba7395132ed744c
Pavel-Pro <pavelpro@gmail.com>
2022-10-27 02:15:32 +0300

    Animate seo text make footer

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $('.category-card__inner').slick({
     infinite: true,
     slidesToShow: 1,
-    slidesToShow: 4
+    slidesToShow: 4,
+    autoplay: true,
+    autoplaySpeed: 1000
 });
 
 let current = Date.now();
@@ -47,3 +49,24 @@ let timer = setInterval(function() {
         $('#timer-secs').prev().find('circle').css('stroke-dashoffset', 282.743 - (secs * 282.743 / 60));
     }
 }, 1000);
+$('.product-submenu__link ').hover(function() {
+    $(this).css("color", "rgb(239, 62, 51)");
+},
+function(){
+    $(this).css("color", "rgb(0, 0, 0)");
+});
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null,    // optional scroll container selector, otherwise use window,
+    resetAnimation: true,     // reset animation on end (default is true)
+  }
+);
